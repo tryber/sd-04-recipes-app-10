45ea4384b5b0fd6675a7f20306ad1f8234ed5d32
Leandro Marten <leandromarten1@gmail.com>
2020-07-30 19:07:04 -0300

    basic structure

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 export default function Login() {
   return (
     <div>
-      <h1>AAAAAAAAAAAAAAAAA</h1>
+      <h1>Login</h1>
+      <input name="email" type="text" value="a" />
+      <input name="password" type="password" value="a" />
+      <button>Entrar</button>
     </div>
   );
 }
