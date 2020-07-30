9762b2af6f903aea3a6e2a8e9f59a047677426c7
procopio420 <lucas-procopio@outlook.com>
2020-07-30 18:34:05 -0300

    CC

--- src/contexts/MeatContext.js
+++ src/contexts/MeatContext.js
@@ -1,8 +1,12 @@
 import React, { createContext } from 'react';
+import PropTypes from 'prop-types';
 
 export const MeatContext = createContext();
-
 
 export default function MeatsProvider({ children }) {
   return <MeatContext.Provider value={[]}>{children}</MeatContext.Provider>;
 }
+
+MeatsProvider.propTypes = {
+  children: PropTypes.any.isRequired,
+};
