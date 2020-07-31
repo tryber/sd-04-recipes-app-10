93e635df8e76919a58666cf3c063bd983158e19e
Kaecio <kaecio@yahoo.com.br>
2020-07-30 22:58:17 -0300

    Testando o req foote, CC

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,13 @@ import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
-    return (
-        <footer>
-            <Link to='/bebidas'>{drinkIcon}</Link>
-            <Link to='/explorar'>{exploreIcon}</Link>
-            <Link to='/comidas'>{mealIcon}</Link>
-        </footer>
-    )
+  return (
+      <footer>
+          <Link to="/bebidas">{drinkIcon}</Link>
+          <Link to="/explorar">{exploreIcon}</Link>
+          <Link to="/comidas">{mealIcon}</Link>
+      </footer>
+    );
 }
 
 export default Footer;
