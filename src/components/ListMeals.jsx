9559b4b86557349b30eec31a5f11d106f3961b3b
Leandro Marten <leandromarten1@gmail.com>
2020-07-31 18:43:39 -0300

    add data-testid

--- src/components/ListMeals.jsx
+++ src/components/ListMeals.jsx
@@ -5,14 +5,14 @@ const ListMeals = () => {
   const [meals] = useMeals();
   return (
     <div className="row">
-      {meals.slice(0, 12).map((meal) => {
+      {meals.slice(0, 12).map((meal, index) => {
         const { strMeal, strMealThumb } = meal;
         return (
-          <div className="col-6 mb-3">
+          <div className="col-6 mb-3" key={index}>
             <div className="card">
-              <img src={strMealThumb} className="card-img-top" alt={strMeal} />
+              <img data-testid={`${index}-card-img`} src={strMealThumb} className="card-img-top" alt={strMeal} />
               <div className="card-body">
-                <h5 className="card-title mb-0">{strMeal}</h5>
+                <h5 className="card-title mb-0" data-testid={`${index}-card-name`}>{strMeal}</h5>
               </div>
             </div>
           </div>
