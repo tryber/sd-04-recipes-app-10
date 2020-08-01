e463a15e8e20c083fd416d71c913705b8b39372c
Kaecio <kaecio@yahoo.com.br>
2020-07-31 23:29:50 -0300

    Filtro categorias, CC maldito, acaba com a vida de qualquer um

--- src/components/CategoryFilters.jsx
+++ src/components/CategoryFilters.jsx
@@ -3,16 +3,16 @@ import useMeals from '../hooks/useMeats';
 import { getCategoriesMeals } from '../services/api';
 
 export default function CategoryFilters() {
-    const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState([]);
 
-  useEffect(() =>{
-    getCategoriesMeals().then(response=>{setCategories(response.meals)})
-},[]);
+  useEffect(() => {
+    getCategoriesMeals().then((response) => { setCategories(response.meals) });
+  }, []);
 
-    return (
-        <div>
-            <button type="button" data-testid={`All-category-filter`} >All</button>
-            {categories.slice(0,5).map(({strCategory }) =>(<button type="button" data-testid={`${strCategory }-category-filter`} >{strCategory }</button>))}
-        </div>
-    )
+  return (
+    <div>
+      <button type='button' data-testid={`All-category-filter`} >All</button>
+      {categories.slice(0, 5).map(({strCategory }) => (<button type="button" data-testid={ `${strCategory }-category-filter`} >{strCategory }</button>))}
+    </div>
+  );
 }
