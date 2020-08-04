41dc78993144f08b1c7d5a2184b7b14473dfb9a8
Kaecio <kaecio@yahoo.com.br>
2020-08-04 17:56:33 -0300

    Requisito 44 e 45 feito, CC

--- src/pages/DetailsMeals.jsx
+++ src/pages/DetailsMeals.jsx
@@ -42,9 +42,7 @@ export default function DetailsMeals() {
             <div>
               <img data-testid="share-btn" src={shareIcon} alt="share" />
               <button onClick={() => setEnableHeart(!enableHeart)} >
-                <img data-testid="favorite-btn" 
-                  src={enableHeart ? blackHearticon : whiteHeartIcon} alt="share"
-                />
+                <img data-testid="favorite-btn" src={enableHeart ? blackHearticon : whiteHeartIcon} alt="share" />
               </button>
             </div>
           </div>
