cebeb1af10b62beafdad069fc976c1ec85ab4686
Kaecio <kaecio@yahoo.com.br>
2020-08-05 18:39:19 -0300

    Tela de receitas feitas, CC

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -49,53 +49,53 @@ export default function DoneRecipes() {
           </div>
           <div className="col">
             <div className="row">
-            <p data-testid={`${index}-horizontal-top-text`} >
+              <p data-testid={`${index}-horizontal-top-text`} >
+              {meal.strCategory}, {meal.strArea}
+            </p>
+              <img data-testid={`${index}-horizontal-share-btn`} src={shareIcon} alt="Share" />
+            </div>
+            <div className="row">
+              <p data-testid={`${index}-horizontal-name`}>
+              {meal.strMeal}
+            </p>
+            </div>
+            <div className="row">
+              <p data-testid={`${index}-horizontal-done-date`}>
+              Feito em 05/08/2020
+            </p>
+            </div>
+            <div data-testid={`${index}-${tagName}-horizontal-tag`} className="row">
+              {getTag(meal.strTags).map((tag) => <p key={tag} className="mr-2">{tag}</p>)}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="card pt-2">
+        <div className="row">
+          <div className="col">
+          <img data-testid={`${index}-horizontal-image`} src={meal.strMealThumb} width="100%" alt="Food" />
+        </div>
+          <div className="col">
+            <div className="row">
+            <p data-testid={`${index}-horizontal-top-text`}>
               {meal.strCategory}, {meal.strArea}
             </p>
             <img data-testid={`${index}-horizontal-share-btn`} src={shareIcon} alt="Share" />
           </div>
             <div className="row">
-            <p data-testid={`${index}-horizontal-name`}>
+            <p data-testid={`${index}-horizontal-name`} >
               {meal.strMeal}
             </p>
           </div>
             <div className="row">
             <p data-testid={`${index}-horizontal-done-date`}>
-              Feito em 05/08/2020
+              Feito em 29/07/2020
             </p>
           </div>
             <div data-testid={`${index}-${tagName}-horizontal-tag`} className="row">
             {getTag(meal.strTags).map((tag) => <p key={tag} className="mr-2">{tag}</p>)}
           </div>
           </div>
-        </div>
-      </div>
-      <div className="card pt-2">
-        <div className="row">
-        <div className="col">
-          <img data-testid={`${index}-horizontal-image`} src={meal.strMealThumb} width="100%" alt="Food" />
-        </div>
-          <div className="col">
-          <div className="row">
-            <p data-testid={`${index}-horizontal-top-text`}>
-              {meal.strCategory}, {meal.strArea}
-            </p>
-            <img data-testid={`${index}-horizontal-share-btn`} src={shareIcon} alt="Share" />
-          </div>
-          <div className="row">
-            <p data-testid={`${index}-horizontal-name`} >
-              {meal.strMeal}
-            </p>
-          </div>
-          <div className="row">
-            <p data-testid={`${index}-horizontal-done-date`}>
-              Feito em 29/07/2020
-            </p>
-          </div>
-          <div data-testid={`${index}-${tagName}-horizontal-tag`} className="row">
-            {getTag(meal.strTags).map((tag) => <p key={tag} className="mr-2">{tag}</p>)}
-          </div>
-        </div>
         </div>
       </div>
     </React.Fragment>
