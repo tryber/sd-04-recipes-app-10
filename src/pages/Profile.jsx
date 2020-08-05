bb131716097d54de7d822bfd7a827f7690c35fb0
Leandro Marten <leandromarten1@gmail.com>
2020-08-05 15:30:27 -0300

    add testids

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,12 +4,13 @@ import HeaderMeals from '../components/HeaderMeals';
 import Footer from '../components/Footer';
 import useAuthentication from '../hooks/useAuthentication';
 
-function createButton(name, link) {
+function createButton(name, link, testid) {
   return (
     <div className="row justify-content-center py-3">
       <Link
         to={link}
         className="btn btn-secondary btn-lg btn-block justify-content-center"
+        data-testid={`profile-${testid}-btn`} 
       >
         {name}
       </Link>
@@ -24,11 +25,11 @@ export default function Profile() {
       <HeaderMeals title="Perfil" />
       <div className="col mt-5 py-">
         <div className="row justify-content-center py-3">
-          <h5 className="">{user.email}</h5>
+          <h5 data-testid="profile-email">{user.email}</h5>
         </div>
-        {createButton('Receitas Feitas', 'receitas-feitas')}
-        {createButton('Receitas Favoritas', 'receitas-favoritas')}
-        {createButton('Sair', '/')}
+        {createButton('Receitas Feitas', 'receitas-feitas', 'done')}
+        {createButton('Receitas Favoritas', 'receitas-favoritas', 'favorite')}
+        {createButton('Sair', '/', 'logout')}
       </div>
       <Footer />
     </React.Fragment>
