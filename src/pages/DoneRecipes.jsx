456cee0182e0973c70386824fbc0ef99af3a4792
Kaecio <kaecio@yahoo.com.br>
2020-08-06 16:05:40 -0300

    Criando componente botao para pg DoneRecipes

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import HeaderMeals from '../components/HeaderMeals';
 import mock from '../services/mock';
 import shareIcon from '../images/shareIcon.svg';
+import useCopy from '../hooks/useCopy';
+import ButtonToggle from '../components/ButtonToggle';
 
 export default function DoneRecipes() {
   const meal = mock[0];
+  const [ message, copy ] =  useCopy(window.location.href)
 
   function getTag(tags) {
     const newTags = tags.split(',');
@@ -15,56 +18,32 @@ export default function DoneRecipes() {
   return (
     <React.Fragment>
       <HeaderMeals title="Receitas Feitas" />;
-
-    <div className="row justify-content-center mb-2 mt-5">
-      <button
-        type="button"
-        className="btn btn-primary btn-sm m-1 w-25"
-        data-testid="All-category-filter"
-        // onClick={() => toggleFilter('All')}
-      >
-        All
-      </button>
-      <button
-        type="button"
-        className="btn btn-primary btn-sm m-1 w-25"
-        data-testid="All-category-filter"
-        // onClick={() => toggleFilter('All')}
-      >
-        Food
-      </button>
-      <button
-        type="button"
-        className="btn btn-primary btn-sm m-1 w-25"
-        data-testid="All-category-filter"
-        // onClick={() => toggleFilter('All')}
-      >
-        Drinks
-      </button>
-    </div>
+        <ButtonToggle />
       <div className="card pt-2">
         <div className="row">
           <div className="col">
-            <img data-testid={`${index}-horizontal-image`} src={meal.strMealThumb} width="100%" alt="Food" />
+            <img data-testid={`-horizontal-image`} src={meal.strMealThumb} width="100%" alt="Food" />
           </div>
           <div className="col">
             <div className="row">
-              <p data-testid={`${index}-horizontal-top-text`} >
+              <p data-testid={`-horizontal-top-text`} >
                 {meal.strCategory}, {meal.strArea}
               </p>
-              <img data-testid={`${index}-horizontal-share-btn`} src={shareIcon} alt="Share" />
+              {message || (<button type="button" onClick={() => copy()}>
+                  <img data-testid={`-horizontal-share-btn`} src={shareIcon} alt="Share" />
+                </button>)}
             </div>
             <div className="row">
-              <p data-testid={`${index}-horizontal-name`}>
+              <p data-testid={`-horizontal-name`}>
                 {meal.strMeal}
               </p>
             </div>
             <div className="row">
-              <p data-testid={`${index}-horizontal-done-date`}>
+              <p data-testid={`-horizontal-done-date`}>
               Feito em 05/08/2020
             </p>
             </div>
-            <div data-testid={`${index}-${tagName}-horizontal-tag`} className="row">
+            <div data-testid={`-horizontal-tag`} className="row">
               {getTag(meal.strTags).map((tag) => <p key={tag} className="mr-2">{tag}</p>)}
             </div>
           </div>
@@ -73,30 +52,42 @@ export default function DoneRecipes() {
       <div className="card pt-2">
         <div className="row">
           <div className="col">
-            <img data-testid={`${index}-horizontal-image`} src={meal.strMealThumb} width="100%" alt="Food" />
+            <img data-testid={`-horizontal-image`} src={meal.strMealThumb} width="100%" alt="Food" />
           </div>
           <div className="col">
             <div className="row">
-              <p data-testid={`${index}-horizontal-top-text`}>
+              <p data-testid={`-horizontal-top-text`}>
                 {meal.strCategory}, {meal.strArea}
               </p>
-              <img data-testid={`${index}-horizontal-share-btn`} src={shareIcon} alt="Share" />
+              {/* <button onClick={() => onClickMessage()}>
+                <img data-testid={`-horizontal-share-btn`} src={shareIcon} alt="Share" />
+              </button> */}
             </div>
             <div className="row">
-              <p data-testid={`${index}-horizontal-name`} >
+              <p data-testid={`-horizontal-name`} >
                 {meal.strMeal}
               </p>
             </div>
             <div className="row">
-              <p data-testid={`${index}-horizontal-done-date`}>
+              <p data-testid={`-horizontal-done-date`}>
               Feito em 29/07/2020
               </p>
             </div>
-            <div data-testid={`${index}-${tagName}-horizontal-tag`} className="row">
+            <div data-testid={`-horizontal-tag`} className="row">
               {getTag(meal.strTags).map((tag) => <p key={tag} className="mr-2">{tag}</p>)}
             </div>
           </div>
         </div>
+      {/* <Modal show={true}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Mensagem
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Boby>
+        Link copiado!
+        </Modal.Boby>
+      </Modal> */}
       </div>
     </React.Fragment>
   );
