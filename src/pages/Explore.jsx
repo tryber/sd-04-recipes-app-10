ef0a2ab572accb1422404d65a26a3cb621f69894
Kaecio <kaecio@yahoo.com.br>
2020-08-06 18:57:07 -0300

    Tela Explore

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 import HeaderMeals from '../components/HeaderMeals';
 import Footer from '../components/Footer';
@@ -6,6 +7,28 @@ export default function Explore() {
   return (
     <React.Fragment>
       <HeaderMeals title="Explorar" />
+      <div className="row justify-content-center align-items-center">
+        <div className="col-12 mt-5 pt-5">
+        <Link to="/comidas" >
+          <button
+            data-testid="explore-food"
+            className="btn btn-secondary btn-block btn-lg" 
+            type="button"
+          >
+            Explorar Comidas
+          </button>
+        </Link>
+        <Link to="/bebidas">
+          <button
+            data-testid="explore-drinks"
+            className="btn btn-secondary btn-block btn-lg"
+            type="button"
+          >
+            Explorar Bebidas
+          </button>
+        </Link>
+        </div>
+      </div>
       <Footer />
     </React.Fragment>
   );
