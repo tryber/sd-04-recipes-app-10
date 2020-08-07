d6e63f620656bf6776c88dd04e61fd6ef2c7bb92
Kaecio <kaecio@yahoo.com.br>
2020-08-06 23:39:45 -0300

    Tela ExploreDrinks

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 import HeaderDrinks from '../components/HeaderDrinks';
 import Footer from '../components/Footer';
@@ -6,6 +7,28 @@ export default function ExploreDrinks() {
   return (
     <React.Fragment>
       <HeaderDrinks title="Explorar Bebidas" />
+      <div className="row justify-content-center align-items-center">
+          <div className="col-12 mt-5 pt-5"> 
+            <Link to="/">
+              <button
+                data-testid="explore-by-ingredient"
+                className="btn btn-secondary btn-block btn-lg"
+                type="button"
+              >
+              Por Ingredientes
+              </button>
+            </Link>
+            <Link to="/">
+              <button
+                data-testid="explore-surprise"
+                className="btn btn-secondary btn-block btn-lg"
+                type="button"
+              >
+              Me Surpreenda!
+              </button>
+            </Link>
+          </div>
+        </div>
       <Footer />
     </React.Fragment>
   );
