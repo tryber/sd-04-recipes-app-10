aea418922a7806578aeb70723c8d35be4b2be381
Lucas Procopio <48629624+procopio420@users.noreply.github.com>
2020-08-07 15:35:40 -0300

    Update ExploreMeals.jsx

--- src/pages/ExploreMeals.jsx
+++ src/pages/ExploreMeals.jsx
@@ -7,37 +7,37 @@ export default function ExploreMeals() {
   return (
     <React.Fragment>
       <HeaderMeals title="Explorar Comidas" />
-        <div className="row justify-content-center align-items-center">
-          <div className="col-12 mt-5 pt-5"> 
-            <Link to="/">
-              <button
-                data-testid="explore-by-ingredient"
-                className="btn btn-secondary btn-block btn-lg"
-                type="button"
-              >
+      <div className="row justify-content-center align-items-center">
+        <div className="col-12 mt-5 pt-5">
+          <Link to="/">
+            <button
+              data-testid="explore-by-ingredient"
+              className="btn btn-secondary btn-block btn-lg"
+              type="button"
+            >
               Por Ingredientes
-              </button>
-            </Link>
-            <Link to="/">
-              <button
-                data-testid="explore-by-area"
-                className="btn btn-secondary btn-block btn-lg"
-                type="button"
-              >
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              data-testid="explore-by-area"
+              className="btn btn-secondary btn-block btn-lg"
+              type="button"
+            >
               Por Local de Origem
-              </button>
-            </Link>
-            <Link to="/">
-              <button
-                data-testid="explore-surprise"
-                className="btn btn-secondary btn-block btn-lg"
-                type="button"
-              >
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              data-testid="explore-surprise"
+              className="btn btn-secondary btn-block btn-lg"
+              type="button"
+            >
               Me Surpreenda!
-              </button>
-            </Link>
-          </div>
+            </button>
+          </Link>
         </div>
+      </div>
       <Footer />
     </React.Fragment>
   );
