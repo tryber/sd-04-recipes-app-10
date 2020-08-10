967243206e17de67399c6cba2a45532265f5c0a3
Leandro Marten <leandromarten1@gmail.com>
2020-08-10 15:18:03 -0300

    oo

--- src/pages/ExploreMeals.jsx
+++ src/pages/ExploreMeals.jsx
@@ -18,27 +18,6 @@ const ExploreMeals = () => {
   return (
     <React.Fragment>
       <HeaderMeals title="Explorar Comidas" />
-<<<<<<< HEAD
-      <div className="row justify-content-center align-items-center">
-        <div className="col-12 mt-5 pt-5">
-          <Link to="/explorar/comidas/ingredientes">
-            <button
-              data-testid="explore-by-ingredient"
-              className="btn btn-secondary btn-block btn-lg mb-4"
-              type="button"
-            >
-              Por Ingredientes
-            </button>
-          </Link>
-          <Link to="/explorar/comidas/area">
-            <button
-              data-testid="explore-by-area"
-              className="btn btn-secondary btn-block btn-lg mb-4"
-              type="button"
-            >
-              Por Local de Origem
-            </button>
-=======
       <div className="row h-100 justify-content-center align-items-center">
         <div className="col">
           <Link
@@ -54,7 +33,6 @@ const ExploreMeals = () => {
             className="btn btn-secondary btn-block btn-lg"
           >
             Por Local de Origem
->>>>>>> 194e7f17d5835250d117b69ede28fc1868cb1c13
           </Link>
           <Link
             to={`/comidas/${recipe.idMeal}`}
